522e16ea1a7f8f87f0cda9099771e258329fee0d
kenisha-star <77574261+kenisha-star@users.noreply.github.com>
2021-02-24 20:46:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-var images1 = [
-  "KENI.jpg,BON.jpg,PAPA.jpg"
+var images1 = ["51KNpAKei-L.jpg",
+"PAPA.jpg",
+"BON.jpg"
 ];
 var i = 0;
-function change() {
- if(i==9)
+function change1() {
+ if(i==3)
  {
    i=0;}
   document.getElementById("i1").src = images1[i];
